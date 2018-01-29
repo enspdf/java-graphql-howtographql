93d923c41e17bbb6c6bbcdd17655d925f1eea44f
sebastian Higuita castañeda <sebastianhiguitaca@gmail.com>
2018-01-28 21:34:21 -0500

    Added filter ability

--- src/main/java/com/howtographql/hackernews/LinkRepository.java
+++ src/main/java/com/howtographql/hackernews/LinkRepository.java
@@ -3,12 +3,14 @@ package com.howtographql.hackernews;
 
 import com.mongodb.client.MongoCollection;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 import org.bson.types.ObjectId;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Filters.*;
 
 public class LinkRepository {
     private final MongoCollection<Document> links;
@@ -17,27 +19,43 @@ public class LinkRepository {
         this.links = links;
     }
 
-    public Link findById (String id) {
+    public Link findById(String id) {
         Document doc = links.find(eq("_id", new ObjectId(id))).first();
         return link(doc);
     }
 
-    public List<Link> getAllLinks () {
-        List<Link> allLinks = new ArrayList<>();
-        for (Document doc : links.find()) {
-            Link link = new Link(
-                    doc.get("_id").toString(),
-                    doc.getString("url"),
-                    doc.getString("description"),
-                    doc.getString("postedBy")
-            );
-            allLinks.add(link);
-        }
+    public List<Link> getAllLinks(LinkFilter filter) {
+        Optional<Bson> mongoFilter = Optional.ofNullable(filter).map(this::buildFilter);
 
+        List<Link> allLinks = new ArrayList<>();
+        for (Document doc : mongoFilter.map(links::find).orElseGet(links::find)) {
+            allLinks.add(link(doc));
+        }
         return allLinks;
     }
 
-    public void saveLink (Link link) {
+    private Bson buildFilter(LinkFilter filter) {
+        String descriptionPattern = filter.getDescriptionContains();
+        String urlPattern = filter.getUrlContains();
+        Bson descriptionCondition = null;
+        Bson urlCondition = null;
+
+        if (descriptionPattern != null && !descriptionPattern.isEmpty()) {
+            descriptionCondition = regex("description", ".*" + descriptionPattern + ".*", "i");
+        }
+
+        if (urlPattern != null && !urlPattern.isEmpty()) {
+            urlCondition = regex("url", ".*" + urlPattern + ".*", "i");
+        }
+
+        if (descriptionCondition != null && urlCondition != null) {
+            return and(descriptionCondition, urlCondition);
+        }
+
+        return descriptionCondition != null ? descriptionCondition : urlCondition;
+    }
+
+    public void saveLink(Link link) {
         Document doc = new Document();
         doc.append("url", link.getUrl());
         doc.append("description", link.getDescription());
@@ -45,7 +63,7 @@ public class LinkRepository {
         links.insertOne(doc);
     }
 
-    private Link link (Document doc) {
+    private Link link(Document doc) {
         return new Link(
                 doc.get("_id").toString(),
                 doc.getString("url"),
